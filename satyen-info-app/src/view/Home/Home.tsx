7c441778d7e9804d76517ca5e72f4d5c4e55c6db
punhale <pankaj.unhale@capgemini.com>
2020-11-29 10:51:24 +0530

    project setup

---
+++ satyen-info-app/src/view/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import logo from '../../assets/app-bg.jpg';
+const Home = () => {
+return <img src={logo} alt='Satyen Info - Computer Sales and Services'/>;
+}
+
+export default Home;
